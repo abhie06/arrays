358de8e5e40a284746536e9e86ba6de09a4df4ae
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2024-08-12 21:21:20 +0530

    added feauture

--- tree.c
+++ tree.c
@@ -49,23 +49,49 @@ int duplicates(int *arr){
 // }
 
 void freq(int *arr){
-    int count;
+    int count, isUnique;
     for(int i=0;i<n;i++){
         count = 0;
+        isUnique = 0;
         for(int j=i+1;j<n;j++){
             if(arr[i]==arr[j]){
+                isUnique = 1;
                 count++;
             }
         }
+        if(isUnique==0){
         printf("\n%d is %d\n",arr[i], count+1);
+        }
+    }
+}
+void ascending(int *arr){
+    for(int i=0;i<n;i++){
+        for(int j= i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                int temp;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;                    
+            }
+        }
+        printf("\n%d ", arr[i]);
+    }
+}
+void insertVal(int *arr, int num, int n){
+    arr[n] = num;
+    n++;
+    for(int i=0;i<n;i++){
+        printf("\n%d ",arr[i]);
     }
 }
 int main(){
     int arr[10];
     insert(arr);
-    int a = sum(arr, n);
-    printf("\n%d\n",a);
-    int c = duplicates(arr);
-    printf("\nThe number of duplicates are: %d\n", c);
-    freq(arr);
+    // int a = sum(arr, n);
+    // printf("\n%d\n",a);
+    // int c = duplicates(arr);
+    // printf("\nThe number of duplicates are: %d\n", c);
+    // freq(arr);
+    // ascending(arr);
+    insertVal(arr, 10, n);
 }
